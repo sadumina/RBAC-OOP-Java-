b649ba497fef25545aa1184840eaff304d3f4be2
sadumina <bagyasadumina@gmail.com>
2025-11-25 15:59:48 +0530

    add ResouceType to User (what to Access)

--- JavaRBAC/src/com/user/rbac/model/ResouceType.java
+++ JavaRBAC/src/com/user/rbac/model/ResouceType.java
@@ -1,5 +1,16 @@
 package com.user.rbac.model;
 
-public class ResouceType {
+/**
+ * Represents different resources that actions can be applied to.
+ * Example: Reports, Users, Vehicles, Orders, Configurations.
+ */
+
+public enum ResouceType {
+
+    REPORT,
+    USER,
+    VECHICLE,
+    ORDER,
+    CONFIG
     
 }
