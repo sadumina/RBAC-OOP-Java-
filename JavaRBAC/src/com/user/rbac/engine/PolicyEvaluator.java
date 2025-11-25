d44cbdeda3d16ede28dd2a0ab1f7bcb00de0497d
sadumina <bagyasadumina@gmail.com>
2025-11-25 16:08:35 +0530

    Adding Evaluator

--- JavaRBAC/src/com/user/rbac/engine/PolicyEvaluator.java
+++ JavaRBAC/src/com/user/rbac/engine/PolicyEvaluator.java
@@ -1,5 +1,21 @@
 package com.user.rbac.engine;
 
-public class PolicyEvaluator {
-    
+/**
+ * PolicyEvaluator defines HOW an access request is evaluated.
+ *
+ * This is an interface, meaning:
+ * - Different authorization strategies can be implemented
+ * - Example: RBAC, ABAC, PBAC, custom rules
+ *
+ * Our main engine (AccessControlEngine) will implement this interface.
+ */
+public interface PolicyEvaluator {
+
+    /**
+     * Evaluates an access request and returns the decision.
+     *
+     * @param request - the user's access request
+     * @return AccessDecision (allowed or denied + reason)
+     */
+    AccessDecision evaluate(AccessRequest request);
 }
